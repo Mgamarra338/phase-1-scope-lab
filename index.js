b441c04364a80c34d55f75e22f8a057a7f8f5da7
Michael Gamarra <Mgamarra908@gmail.com>
2023-08-13 21:08:00 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'bob';
+    leastFavoriteCustomer = 'tom';
+}
